745cac6bddad9676b9e23ddeaf842ffd706268cd
HaraYoung <qkrtpdud9899@gmail.com>
2022-06-29 16:40:55 +0900

    test

--- cply-mall/src/App.js
+++ cply-mall/src/App.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-    </div>
-  )
-=======
+
 import NoticeMain from "./pages/YS/NoticeMain";
 import { Routes, Route } from "react-router-dom";
 import NoticeContent from "./pages/YS/NoticeContent";
@@ -20,7 +14,6 @@ function App() {
       </Routes>
     </>
   );
->>>>>>> ce779b27b226730ff7584bdc99ba6a82c08e18b7
 }
 
 export default App;
