b8159217606f7a79b2dc1d57fded89e862e7e60a
HaraYoung <qkrtpdud9899@gmail.com>
2022-07-14 15:12:06 +0900

    ..

--- cply-mall/src/pages/GH/MyPage.js
+++ cply-mall/src/pages/GH/MyPage.js
@@ -38,7 +38,7 @@ const MyPage = () => {
           <Route path="order" element={<Order />} />
           <Route path="orderdetail" element={<OrderDetail />} />
           <Route path="cart" element={<Cart />} />
-          <Route path="rande" element={<RandE />} />
+          <Route path="rande/*" element={<RandE />} />
           <Route path="likes/*" element={<Likes />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
