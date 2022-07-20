a93c475786fb75d13bb167bec6e01302f66614f9
sujin <sujin971008@naver.com>
2022-07-20 18:26:56 +0900

    0720

--- cply-mall/src/pages/SJ/myReview.js
+++ cply-mall/src/pages/SJ/myReview.js
@@ -1,24 +1,20 @@
 // 검색창에 원하는 검색어를 넣었을 때 나오는 결과
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
 import DateSearch from "./components/dateSearch";
 import ReviewList from "./components/reviewList";
 import styled from "styled-components";
 
-const ContentContainer = styled.section`
+const Review = styled.div`
   width: 1200px;
-  margin: 0 auto;
+  margin: auto;
 `;
 
 const MyReview = () => {
   return (
-    <ContentContainer>
-      <Header />
+    <Review>
       <DateSearch />
       <ReviewList />
-      <Footer />
-    </ContentContainer>
+    </Review>
   );
 };
 
